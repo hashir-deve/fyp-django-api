a1e45827b1d8d86c6977ea80e2cf1210802fa86e
hashir0981 <muhammad.hashir@algo.com>
2024-06-27 04:53:20 +0500

    Added code for Chatbot and Food Portion Estimation

---
+++ core/gemini_helper.py
@@ -0,0 +1,75 @@
+import os
+
+from dotenv import load_dotenv
+import google.generativeai as genai
+import google.generativeai as genai
+from IPython.display import Image
+
+# Used to securely store your API key
+
+from loguru import logger
+
+load_dotenv()
+ 
+GOOGLE_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GOOGLE_API_KEY)
+logger.debug("Available Gemini Models")
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+
+model = genai.GenerativeModel('models/gemini-1.5-pro-latest')
+
+def call_gemini(query):
+            #    You will be asked a question. Your reply should include a 
+            #    descriptive paragraph without any heading or bold text.
+   query = f"""
+               You will be asked a question. Your reply should not include Answer heading.
+               Answer questions only related to fitness and nutrition domain, 
+               if the following Question belong to other domain except the fitness, 
+               please respond with a message that you can only answer queries related to above listed domain.
+               Question: {query}
+           """
+   response = model.generate_content(query)
+   return (response.text)
+
+def predict_image_using_gemini(file_path):
+    sample_file = genai.upload_file(path=file_path, display_name="Sample drawing")
+
+    print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
+
+    file = genai.get_file(name=sample_file.name)
+    print(f"Retrieved file '{file.display_name}' as: {sample_file.uri}")
+
+    response = model.generate_content(
+        ["""Predict only name and calories of the Fruit or Vegetable without any description in image as illustrated below.
+            name: , calories:
+         """, sample_file]
+    )
+
+    predicted_properties = response.text
+
+    # Split the string by comma
+    items = predicted_properties.split(',')
+
+    # Initialize an empty dictionary
+    food_dict = {}
+
+    # Loop through each item in the list
+    for item in items:
+        # Split each item by colon and strip any leading/trailing whitespace
+        key, value = item.split(':')
+        key = key.strip()
+        value = value.strip()
+
+        # Add the key-value pair to the dictionary
+        food_dict[key] = value
+
+    try:
+        os.remove(file_path)
+        print("File Deleted Successfully at Path: ", file_path)
+    except Exception as e:
+        raise e
+
+    return food_dict
---
+++ core/views.py
@@ -0,0 +1,80 @@
+
+from loguru import logger
+from rest_framework import decorators, response, status
+
+from core.gemini_helper import call_gemini, predict_image_using_gemini
+
+import os
+import time
+from django.http import HttpResponse
+from django.conf import settings
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+
+@decorators.api_view(['GET'])
+# @timeit("endpoints.ttfb.autocomplete_message")
+def process_message(request):
+    try:
+        message = request.query_params['query']
+        logger.debug("Asked Question: ", message)
+
+        query_result = call_gemini(message)
+        
+        return response.Response({
+            'response': query_result
+        })
+    except Exception as e:
+        logger.error(e)
+        return response.Response(
+                {
+                    'status_code' : status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    'message' : e,
+                    'exception_class' : e.__class__.__name__,
+                    'exception_message' : e.args[0]
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    
+
+@decorators.api_view(['POST'])
+# @timeit("endpoints.ttfb.autocomplete_message")
+def predict_image(request):
+    try:
+        # message = request.query_params['query']
+        # logger.debug("Asked Question: ", message)
+        
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_path = handle_uploaded_file(request.FILES['file'])
+            print(f"File uploaded successfully. File path: {file_path}")
+    
+        query_result = predict_image_using_gemini(file_path)
+        
+        return response.Response(
+            query_result
+        )
+    except Exception as e:
+        logger.error(e)
+        return response.Response(
+                {
+                    'status_code' : status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    'message' : e,
+                    'exception_class' : e.__class__.__name__,
+                    'exception_message' : e.args[0]
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+def handle_uploaded_file(f):
+    timestamp = int(time.time())
+    filename = f"{timestamp}_{f.name}"
+    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+
+    with open(file_path, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+    print(f"Absolute file path: {file_path}")
+    return file_path
+
